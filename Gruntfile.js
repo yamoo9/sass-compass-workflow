cf4855610e0e9193abdb50f7897f90bd178124a3
yamoo9 <yamoo9@naver.com>
2014-10-17 15:41:24 +0900

    grunt test

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.initConfig({
+		'uglify': {
+			my_target: {
+				files: {
+					 '_/js/script.js', ['_/components/js/script.js']
+				} // files
+			} // my_target
+		} // uglify
+	}); // initConfig
+
+}; // exoprts
---
+++ _/components/js/script.js
@@ -0,0 +1,7 @@
+(function(global){
+	function test() {
+		return 'hello';
+	}
+
+	test();
+}(window))
